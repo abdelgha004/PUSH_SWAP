521714e6f5e38da286d831165971134d8ec22848
aakourya <abdelghafourakouryah@gmail.com>
2026-01-14 17:06:39 +0100

    finish

--- sort/sort_large.c
+++ sort/sort_large.c
@@ -6,7 +6,7 @@
 /*   By: aakourya <aakourya@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/08 08:27:22 by aakourya          #+#    #+#             */
-/*   Updated: 2026/01/09 16:18:00 by aakourya         ###   ########.fr       */
+/*   Updated: 2026/01/14 17:03:24 by aakourya         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,22 +15,22 @@
 void	sort_large_stack(t_list **stack_a, t_list **stack_b, int size)
 {
 	int	chunk_limit;
-	int	i;
+	int	pushed;
 
-	i = 0;
+	pushed = 0;
 	chunk_limit = 30;
 	while (*stack_a)
 	{
-		if ((*stack_a)->index <= i)
+		if ((*stack_a)->index <= pushed)
 		{
 			push_b(stack_b, stack_a, 1);
-			i++;
+			pushed++;
 		}
-		else if ((*stack_a)->index < i + chunk_limit)
+		else if ((*stack_a)->index < pushed + chunk_limit)
 		{
 			push_b(stack_b, stack_a, 1);
 			rotate_b(stack_b, 1);
-			i++;
+			pushed++;
 		}
 		else
 			rotate_a(stack_a, 1);
